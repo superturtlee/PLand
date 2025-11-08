0078de6ea053a63202373d0981433abc46ad9b6c
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-11-08 09:19:47 +0800

    chore:更新配置版本

--- src/pland/infra/Config.h
+++ src/pland/infra/Config.h
@@ -18,7 +18,7 @@ struct ForbiddenRange {
 };
 
 struct Config {
-    int              version{30};
+    int              version{31};
     ll::io::LogLevel logLevel{ll::io::LogLevel::Info};
 
     EconomyConfig economy;
